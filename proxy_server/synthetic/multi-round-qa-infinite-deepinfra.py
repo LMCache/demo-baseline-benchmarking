548701dd2bb4ce65b1e11d608a54a5973fbbb1e7
zhuohangu <122142258+zhuohangu@users.noreply.github.com>
2025-09-26 14:24:17 -0400

    Remove dummy

--- proxy_server/synthetic/multi-round-qa-infinite-deepinfra.py
+++ proxy_server/synthetic/multi-round-qa-infinite-deepinfra.py
@@ -135,7 +135,7 @@ class RequestExecutor:
         # if not base_url.endswith('/v1'):
         #     base_url = base_url.rstrip('/') + '/v1'
         self.client = openai.AsyncOpenAI(
-            api_key="1Ft8yuNBUkITVoVbCpyMMIsqglirxxpr",  # Dummy API key for vLLM server
+            api_key="",  # Dummy API key for vLLM server
             base_url=base_url
         )
         self.model = model
